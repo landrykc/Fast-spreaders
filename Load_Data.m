a1b85ecfe3f78b8acc147394eee057d070b8dd4b
Adam <wangagk@bc.edu>
2021-04-21 13:31:39 +0800

    Update & Debug
    Solved problems with 'Load_Data.m'.
    Should work perfectly now.

--- Load_Data.m
+++ Load_Data.m
@@ -1,3 +1,7 @@
+%% EESC6664 - Environmental Data Exploration & Analysis
+% Team project
+% Char, Sky, Kameko, Adam
+
 %% Load Data Files
 
 clear;
@@ -5,14 +9,14 @@ clc;
 
 addpath('Data Files');
 
-%EPR Data along 9N includes:
+% EPR (East Pacific Rise) Data along 9N includes:
 %       Leg 138 density & P wave velocity
 %       Leg 201 density & P wave velocity
 %       Heat flow
 %       Locations of Earthquakes >5.0
 %       Whole Rock, Glass, and Mineral Chem Data
 
-%MAR Ridge Data
+% MAR (Mid Atlantic Ridge) Data:
 %       Leg 306 density & P wave velocity
 %       Heat flow
 %       Locations of Earthquakes >0
@@ -21,65 +25,69 @@ addpath('Data Files');
 %% Load Density Files
 % Database: IODP
 
-filename = 'Leg_138_density_EPR.csv';
-EPR_density_138 = readtable(filename);
+% EPR - Leg 138 & 201
+EPR_density_138 = readtable('Leg_138_density_EPR.csv');
+EPR_density_201 = readtable('Leg_201_density_EPR.csv');
 
-filename = 'Leg_201_density_EPR.csv';
-EPR_density_201 = readtable(filename);
-
-filename = 'Leg_306_density_MAR.csv';
-MAR_density_306 = readtable(filename);
-
+% MAR - Leg 306
+MAR_density_306 = readtable('Leg_306_density_MAR.csv');
 
 %% Load P-Wave Velocity Files
-%Do we want s-wave too?
-%Database: IODP
+% Do we want s-wave too?
+% Database: IODP
 
-filename = 'Leg_138_PWV_EPR.csv';
-EPR_PWV_138 = readtable(filename);
+% EPR - Leg 138 & 201
+EPR_PWV_138 = readtable('Leg_138_PWV_EPR.csv');
+EPR_PWV_201 = readtable('Leg_201_PWV_EPR.csv');
 
-filename = 'Leg_201_PWV_EPR.csv';
-EPR_PWV_201 = readtable(filename);
-
-filename = 'Leg_306_PWV_MAR.csv';
-MAR_PWV_306 = readtable(filename);
-
+% MAR - Leg 306
+MAR_PWV_306 = readtable('Leg_306_PWV_MAR.csv');
 
 %% Load Heat Flow Data
 % rate of thermal energy transfer
 % Collected on Geomapapp, I can't actually find a database for this one
 
+% EPR
 EPR_Heat_Flow_Data = readtable('Global_Heat_Flow_Data_-_Abbott_Compilation_EPR.xlsx');
 
+% MAR
 MAR_Heat_Flow_Data = readtable('Global_Heat_Flow_Data_-_Abbott_Compilation_MAR.xlsx');
 
 %% Load Core Depth Data 
 % We may want different depth data idk
 % Database: IODP, collected on Geomapapp
 
-EPR_Core_Data = readtable('Measurements_on_Cores_DSDP_ODP_and_IODP_Legs_1_to_312_EPR.xlsx');
-%why is this not working
+% EPR
+EPR_Core_Data = readtable('Measurements_on_Cores_DSDP_ODP_and_IODP_Legs_1_to_312_EPR.xls');
 
+% MAR
 MAR_Core_Data = readtable('Measurements_on_Cores_DSDP_ODP_and_IODP_Legs_1_to_312_MAR.xlsx');
 
-
 %% Load Earthquake Data
-%Databse: USGS, collected on Geomapapp
+% Databse: USGS, collected on Geomapapp
 
+% EPR
 EPR_Earthquake_Data = readtable('Magnitude_5.0_(1960-2020)_EPR.xlsx');
 
+% MAR
 MAR_Earthquake_Data = readtable('Magnitude_5.0_(1960-2020)_MAR.xlsx');
 
 %% Chem Data
 % Dataabase: PetDB
 
+% EPR
 EPR_Chem_Data = readtable('EastPacificRise_ALL_CHEM_DATA');
 
-MAR_Chem_Data = load('MAR_Chem_Data_WholeRock.txt');
-%also not working
+% MAR
+MAR_Chem_Data = readtable('MAR_Chem_Data_WholeRock.txt');
 
+%% Save Workspace
 
+% save all_data.mat
 
+% This only needs to be done once
+% Next time simply use 'load all_data.mat'
 
+% Rerun this step if updated
 
-
+%% (Additional Data)
